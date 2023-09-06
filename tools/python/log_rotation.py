1009340fce945243eada41a2188d982c96ba8c95
MACHETE VAULT <142649592+machetevault@users.noreply.github.com>
2023-09-05 22:32:59 -0700

    Create log_rotation.py

---
+++ tools/python/log_rotation.py
@@ -0,0 +1,24 @@
+#!/bin/python3
+
+import shutil
+from datetime import datetime
+
+naming = "_COMPLETED.csv"
+path_rotation = "/home/user/archive" # please change the path to your convenience
+data = "/home/user/file.csv" # please change the path to your convenience
+
+def get_date(): # getting the date as string to add it to the name of the file
+  current = datetime.now().date()
+  str_date = str(current)
+  return str_date
+
+def log_rotation(): # calling get date string, create new naming convention and move the file
+  date = get_date()
+  file_compose = path_rotation + date + naming
+  shutil.move(data,file_compose)
+
+def main():
+  LR = log_rotation
+  LR()
+
+main()
