294caa6ceb8d43a9976f8741dcff372cba6b1252
MACHETE VAULT <142649592+machetevault@users.noreply.github.com>
2023-09-20 21:31:20 -0700

    Update machete_conf.js

--- conf/machete_conf.js
+++ conf/machete_conf.js
@@ -4,8 +4,6 @@
 # @machetevault
 # 
 #
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
   
 # copy commands to clipboard
 function copy_To_Clipboard(element) {
