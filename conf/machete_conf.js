3ec83ec3a86be74441fda49558fe13a33a79cfde
MACHETE VAULT <142649592+machetevault@users.noreply.github.com>
2023-10-12 00:23:20 -0700

    Update machete_conf.js

--- conf/machete_conf.js
+++ conf/machete_conf.js
@@ -4,7 +4,7 @@ function ctrl_C(element) {
   $temp.val($(element).text()).select();
   document.execCommand("copy");
   $temp.remove();
-  window.alert("copied!!\n@machetevault");
+  window.alert("copied to clipboard!!");
 }
 
 
