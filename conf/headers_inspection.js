45960c76f350a934bf04398c2e8ea12b8876546f
MACHETE VAULT <142649592+machetevault@users.noreply.github.com>
2024-10-08 22:37:03 -0600

    Update headers_inspection.js

--- conf/headers_inspection.js
+++ conf/headers_inspection.js
@@ -85,179 +85,355 @@ Attack Vectors: Primarily informational; potential misconfigurations could affec
 		option9: `
 sample:
 	CF-Ray: 8cf25e9c0d082227-MIA
+ 
+Usage: Cloudflare-specific ID for tracing requests.
+
+Attack Vectors: Used for diagnostics; no direct risk.
+ 
 `,
 		option10: `
 sample:
 	Connection: keep-alive
+
+Usage: Controls whether the network connection stays open after the current transaction.
+
+Attack Vectors: Connection header manipulation might be used in HTTP request smuggling.
+ 
 `,
     option11: `
 sample:
-	
+	Content-Disposition:
+ 
+Usage: Suggests a default filename for saving a file.
+
+Attack Vectors: Poor validation could lead to directory traversal attacks.
 
 `,
 		option12: `
-  sample:
+sample:
   	Content-Length: 57
+
+Usage: Indicates the size of the request or response body in bytes.
+
+Attack Vectors: Can be exploited in HTTP request smuggling attacks.
+   
 `,
 		option13: `
 sample:
   	content-security-policy: default-src 'self';base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;
    frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src 'self';script-src-attr 'none';
    style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests
+
+Usage: Mitigates XSS and other injection attacks by specifying allowed content sources.
+
+Attack Vectors: Misconfiguration can reduce security effectiveness.
+   
 `,
     option14: `
 sample:
-	Content-Security-Policy-Report-Only: script-src 'none'; connect-src 'none'; report-uri https://csp-reporting.cloudflare.com/cdn-cgi/script_monitor/report?m=j5xV2lxtvipZVAgy5s.xYjPLn3FMt2Bw8cJ1RDrBuPI-1728354323-1.0.1.1-gBE1R.ecQ2K3TirADxzDbsXOU9MQ9iVRaiFropuXzmqmHViFhyzCi9BynybHXR_311UsOR7AjtMO5e.WNEVFbgZqZHOmjLWYLmbuCf5zv.ZywNyXdSrgxvJ1hU8Ay6q6DjtvkD4uUvngwje.Omzg.vKXG_Qb7W8cgLE0hF5EO0g; 
- report-to cf-csp-endpoint
+	Content-Security-Policy-Report-Only: script-src 'none'; connect-src 'none'; report-uri 
+ https://csp-reporting.cloudflare.com/cdn-cgi/script_monitor/report?m=j5xV2lxtvipZVAgy5s.xYjPLn3FMt2Bw8cJ1RDrBuPI-1728354323
+ -1.0.1.1-gBE1R.ecQ2K3TirADxzDbsXOU9MQ9iVRaiFropuXzmqmHViFhyzCi9BynybHXR_311UsOR7AjtMO5e.WNEVFbgZqZHOmjLWYLmbuCf5zv.ZywNyXdSr
+ gxvJ1hU8Ay6q6DjtvkD4uUvngwje.Omzg.vKXG_Qb7W8cgLE0hF5EO0g; report-to cf-csp-endpoint
+
+Usage: Similar to CSP, but only reports violations without enforcing them.
+
+Attack Vectors: Used for testing policies before enforcement.
+ 
 `,
 		option15: `
 sample:
 	Content-Type: application/json; charset=utf-8
+
+Usage: Specifies the media type of the resource or request body.
+
+Attack Vectors: Incorrect handling can lead to MIME type confusion attacks.
+ 
 `,
 		option16: `
 sample:
 	Cookie: _ga_W7DW3R7679=GS1.1.1728331726.2.0.1728331726.0.0.0; _ga=GA1.1.1721535675.1728328576;
+
+Usage: Stores session data on the client.
+
+Attack Vectors: Susceptible to session hijacking or fixation if not secured properly.
+ 
 `,
     option17: `
 sample:
 	cross-origin-opener-policy: same-origin
+
+Usage: Controls sharing of browsing contexts, protecting against cross-origin threats.
+
+Attack Vectors: Misconfiguration could lead to cross-origin attacks.
+ 
 `,
 		option18: `
 sample:
 	cross-origin-resource-policy: same-origin
+
+Usage: Prevents other origins from accessing resources.
+
+Attack Vectors: Misconfiguration can expose resources to unauthorized access.
+ 
 `,
 		option19: `
 sample:
 	Date: Tue, 08 Oct 2024 02:25:23 GMT
+
+Usage: Represents the date and time at which the message was sent.
+
+Attack Vectors: Generally low risk, used for caching and logging.
+ 
 `,
     option20: `
 sample: 
 	ETag: W/"fe-HXRZFMMFoUSWc30W70zcP6Qm7LE"
+
+Usage: Provides a mechanism for cache validation.
+
+Attack Vectors: Can be used for tracking users across sessions.
+ 
 `,
 		option21: `
 sample:
 	expect-ct: max-age=0
+
+Usage: Instructs browsers to enforce Certificate Transparency.
+
+Attack Vectors: Helps detect misissued certificates but not directly exploitable
+ 
 `,
 		option22: `
 sample:
 	If-None-Match: W/"1b1-GFzCGQqk9c7puNvL7NxWzMVJAR8"
+
+Usage: Makes a request conditional based on ETag values.
+
+Attack Vectors: Potentially used for cache poisoning if not handled correctly
+ 
 `,
     option23: `
 sample:
 	Origin: https://hackerone.com
+
+Usage: Indicates the origin of the request, used in CORS.
+
+Attack Vectors: Can be spoofed in CORS attacks.
+ 
 `,
 		option24: `
 sample:
 	origin-agent-cluster: ?1
+
+Usage: Controls whether an origin should have its own agent cluster.
+
+Attack Vectors: Misconfiguration can affect resource isolation.
+ 
 `,
 		option25: `
 sample:
 	Referer: https://hackerone.com/
+
+Usage: Indicates the URL of the resource from which the request was initiated.
+
+Attack Vectors: Can leak sensitive information if URLs contain sensitive data.
+ 
 `,
     option26: `
 sample:
 	Referrer-Policy: strict-origin-when-cross-origin
+
+Usage: Governs what referrer information should be included with requests.
+
+Attack Vectors: Incorrect settings can lead to information leakage
+ 
 `,
 		option27: `
 sample:
-	Report-To: {"endpoints":[{"url":"https:\/\/csp-reporting.cloudflare.com\/cdn-cgi\/script_monitor\/report?m=j5xV2lxtvipZVAgy5s.xYjPLn3FMt2Bw8cJ1RDrBuPI-1728354323-1.0.1.1-gBE1R.ecQ2K3TirADxzDbsXOU9MQ9iVRaiFropuXzmqmHViFhyzCi9BynybHXR_311UsOR7AjtMO5e.WNEVFbgZqZHOmjLWYLmbuCf5zv.ZywNyXdSrgxvJ1hU8Ay6q6DjtvkD4uUvngwje.Omzg.vKXG_Qb7W8cgLE0hF5EO0g"}],"group":"cf-csp-endpoint","max_age":86400}
+	Report-To: {"endpoints":[{"url":"https:\/\/csp-reporting.cloudflare.com\/cdn-cgi\/script_monitor\/report?m=
+ j5xV2lxtvipZVAgy5s.xYjPLn3FMt2Bw8cJ1RDrBuPI-1728354323-1.0.1.1-gBE1R.ecQ2K3TirADxzDbsXOU9MQ9iVRaiFropuXzmqmHViFhyzCi9
+ BynybHXR_311UsOR7AjtMO5e.WNEVFbgZqZHOmjLWYLmbuCf5zv.ZywNyXdSrgxvJ1hU8Ay6q6DjtvkD4uUvngwje.Omzg.vKXG_Qb7W8cgLE0hF5EO0g"}],
+ "group":"cf-csp-endpoint","max_age":86400}
+
+Usage: Specifies reporting endpoints for various browser policies.
+
+Attack Vectors: Misuse could lead to excessive or misdirected reporting.
+
 `,
 		option28: `
 sample:
 	Sec-Fetch-Dest: document
+
+Usage: Describes the destination of the request (e.g., document, script).
+
+Attack Vectors: Provides context for request origins; low direct risk
+ 
 `,
     option29: `
 sample:
 	Sec-Fetch-Mode: navigate
+
+Usage: Describes the mode of the request (e.g., cors, no-cors).
+
+Attack Vectors: Helps enforce security policies; low direct risk
+ 
 `,
 		option30: `
 sample:
 	Sec-Fetch-Site: none
+
+Usage: Indicates the relationship between the origin of the request and the target.
+
+Attack Vectors: Used in security checks; generally low risk
+ 
   `,
 		option31: `
 sample:
 	Sec-Fetch-User: ?1
+
+Usage: Indicates user activation for a navigation request.
+
+Attack Vectors: Provides context for user interactions; low direct risk
+ 
   `,
     option32: `
 sample:
 	Server: cloudflare
+
+Usage: Discloses information about the software used by the origin server.
+
+Attack Vectors: Can provide attackers with information useful for targeting.
     
   `,
 		option33: `
 sample:
-	
+	Set-Cookie:
+ 
+Usage: Sends cookies from server to client, used for session management.
 
+Attack Vectors: Vulnerable to session hijacking if not properly secured.
   
   `,
 		option34: `
 sample:
 	Strict-Transport-Security: max-age=63072000; includeSubDomains
+
+Usage: Enforces HTTPS, protecting against man-in-the-middle attacks.
+
+Attack Vectors: Lack of HSTS can lead to downgrade attacks
   
   `,
     option35: `
 sample:
 	Upgrade-Insecure-Requests: 1
+
+Usage: Informs the server that the client prefers an encrypted and authenticated response.
+
+Attack Vectors: Generally low risk, but ignored by non-compliant servers
     
   `,
     option36: `
 sample:
 	User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0
+
+Usage: Identifies the client software initiating the request.
+
+Attack Vectors: Can be spoofed for bypassing client-specific restrictions
     
   `,
 		option37: `
 sample:
 	Vary: Accept-Encoding
+
+Usage: Informs caches which headers to consider when serving cached content.
+
+Attack Vectors: Misuse can lead to cache poisoning.
   
   `,
 		option38: `
 sample:
 	X-Content-Type-Options: nosniff
+
+Usage: Prevents MIME type sniffing, protecting against some XSS attacks.
+
+Attack Vectors: Lack of this header can lead to content-type sniffing vulnerabilities.
   
   `,
     option39: `
 sample:
-	
+	X-CSRF-Token:
+ 
+Usage: Used to prevent Cross-Site Request Forgery (CSRF) attacks.
+
+Attack Vectors: If not implemented correctly, CSRF attacks remain possible.
     
   `,
 		option40: `
 sample:
 	x-dns-prefetch-control: off
+
+Usage: Controls DNS prefetching, which can improve performance.
+
+Attack Vectors: Generally low risk, but can be used for tracking.
   
   `,
 		option41: `
 sample:
 	x-download-options: noopen
+
+Usage: Used to prevent file downloads from being rendered in the browser.
+
+Attack Vectors: Lack of this header can lead to drive-by download attacks.
+ 
   `,
     option42: `
 sample:
-	X-Frame-Options: SAMEORIGIN
+	x-frame-options: SAMEORIGIN
+
+Usage: Protects against clickjacking by controlling whether a page can be framed.
+
+Attack Vectors: Lack of this header can lead to clickjacking vulnerabilities.
+
   `,
 		option43: `
 sample:
 	x-permitted-cross-domain-policies: none
+
+Usage: Controls loading of cross-domain policies, preventing data theft via Flash or PDF files.
+
+Attack Vectors: Improper settings can lead to unauthorized data access.
   
   `,
 		option44: `
+sample:
+	X-Request-ID:
+ 
+Usage: Used for tracking and debugging requests.
+
+Attack Vectors: Generally low risk; mainly for logging.
   
   `,
-        option45: `
-
-        
-    `,
-
-		option46: `
+		option45: `
 sample:
-	X-XSS-Protection: 0
+	x-xss-protection: 0
+
+Usage: The HTTP X-XSS-Protection response header was a feature of Internet Explorer, Chrome and Safari that stopped pages 
+from loading when they detected reflected cross-site scripting (XSS) attacks. These protections are largely unnecessary in 
+modern browsers when sites implement a strong Content-Security-Policy that disables the use of inline JavaScript ('unsafe-inline').
+
+Attack Vectors: Modern browsers may ignore this header; insufficient on its own to prevent XSS.
+ 
   `,
-    option47: `
+    option46: `
+  `,
+		option47: `
+  
   `,
 		option48: `
   
   `,
-		option49: `
-  
-  `,
-        option50: `
+        option49: `
         
     `
 
