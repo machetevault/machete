96bf20c1cbe7fe2ec0387c097245e3e816c265c1
MACHETE VAULT <142649592+machetevault@users.noreply.github.com>
2024-10-15 00:07:03 -0600

    Update headers_inspection.js

--- conf/headers_inspection.js
+++ conf/headers_inspection.js
@@ -5,7 +5,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
 const largeTexts = {
     option1: `
 sample: 
-	Host: machetevault.com
+	host: machetevault.com
 
 Usage: The Host request header specifies the host and port number of the server to which the request is being sent.
 If no port is included, the default port for the service requested is implied (e.g., 443 for an HTTPS URL, and 80 for an 
@@ -16,7 +16,7 @@ Attack Vectors: Host header injection could lead to cache poisoning or request s
 `,
     option2: `
 sample: 
-	Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8
+	accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8
 
 Usage: The Accept request HTTP header indicates which content types, expressed as MIME types, the client is able to 
 understand. The server uses content negotiation to select one of the proposals and informs the client of the choice 
@@ -27,7 +27,7 @@ Attack Vectors: Improper handling may lead to content type sniffing vulnerabilit
 `,
 		option3: `
 sample: 
-	Accept-Encoding: gzip, deflate, br
+	accept-encoding: gzip, deflate, br
 
 Usage: The Accept-Encoding request HTTP header indicates the content encoding (usually a compression algorithm) that the 
 client can understand. The server uses content negotiation to select one of the proposals and informs the client of that 
@@ -37,7 +37,7 @@ Attack Vectors: Can be exploited in certain compression attacks like BREACH.
 `,
 		option4: `
 sample: 
-	Accept-Language: en-US,en;q=0.5
+	accept-language: en-US,en;q=0.5
 
 Usage: The Accept-Language request HTTP header indicates the natural language and locale that the client prefers. 
 The server uses content negotiation to select one of the proposals and informs the client of the choice with the 
@@ -50,7 +50,7 @@ Attack Vectors: Minimal direct risk, but may be used for fingerprinting users.
 `,
 		option5: `
 sample:
-	Access-Control-Allow-Origin: *
+	access-control-allow-origin: *
 
 Usage:The Access-Control-Allow-Origin response header indicates whether the response can be shared with requesting 
 code from the given origin.
@@ -59,7 +59,7 @@ Attack Vectors: Misconfiguration can lead to CORS vulnerabilities.
 `,
 		option6: `
 sample:
-	Cache-Control: no-cache
+	cache-control: no-cache
 
 Usage: The Cache-Control HTTP header field holds directives (instructions) — in both requests and responses — that control 
 caching in browsers and shared caches (e.g. Proxies, CDNs).
@@ -76,7 +76,7 @@ Attack Vectors: Generally low risk; specific to Cloudflare configurations.
 `,
     option8: `
 sample:
-	CF-Cache-Status: DYNAMIC
+	cf-cache-status: DYNAMIC
 
 Usage: Cloudflare header indicating cache status of a response.
 
@@ -84,7 +84,7 @@ Attack Vectors: Primarily informational; potential misconfigurations could affec
 `,
 		option9: `
 sample:
-	CF-Ray: 8cf25e9c0d082227-MIA
+	cf-ray: 8cf25e9c0d082227-MIA
  
 Usage: Cloudflare-specific ID for tracing requests.
 
@@ -93,7 +93,7 @@ Attack Vectors: Used for diagnostics; no direct risk.
 `,
 		option10: `
 sample:
-	Connection: keep-alive
+	connection: keep-alive
 
 Usage: Controls whether the network connection stays open after the current transaction.
 
@@ -102,7 +102,7 @@ Attack Vectors: Connection header manipulation might be used in HTTP request smu
 `,
     option11: `
 sample:
-	Content-Disposition:
+	content-disposition:
  
 Usage: Suggests a default filename for saving a file.
 
@@ -111,7 +111,7 @@ Attack Vectors: Poor validation could lead to directory traversal attacks.
 `,
 		option12: `
 sample:
-  	Content-Length: 57
+  	content-length: 57
 
 Usage: Indicates the size of the request or response body in bytes.
 
@@ -120,7 +120,7 @@ Attack Vectors: Can be exploited in HTTP request smuggling attacks.
 `,
 		option13: `
 sample:
-   content-security-policy: default-src 'self';base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;
+   	content-security-policy: default-src 'self';base-uri 'self';block-all-mixed-content;font-src 'self' https: data:;
    frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src 'self';script-src-attr 'none';
    style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests
 
@@ -131,7 +131,7 @@ Attack Vectors: Misconfiguration can reduce security effectiveness.
 `,
     option14: `
 sample:
- Content-Security-Policy-Report-Only: script-src 'none'; connect-src 'none'; report-uri 
+ 	content-security-policy-report-only: script-src 'none'; connect-src 'none'; report-uri 
  https://csp-reporting.cloudflare.com/cdn-cgi/script_monitor/report?m=j5xV2lxtvipZVAgy5s.xYjPLn3FMt2Bw8cJ1RDrBuPI-1728354323
  -1.0.1.1-gBE1R.ecQ2K3TirADxzDbsXOU9MQ9iVRaiFropuXzmqmHViFhyzCi9BynybHXR_311UsOR7AjtMO5e.WNEVFbgZqZHOmjLWYLmbuCf5zv.ZywNyXdSr
  gxvJ1hU8Ay6q6DjtvkD4uUvngwje.Omzg.vKXG_Qb7W8cgLE0hF5EO0g; report-to cf-csp-endpoint
@@ -143,7 +143,7 @@ Attack Vectors: Used for testing policies before enforcement.
 `,
 		option15: `
 sample:
-	Content-Type: application/json; charset=utf-8
+	content-type: application/json; charset=utf-8
 
 Usage: Specifies the media type of the resource or request body.
 
@@ -152,7 +152,7 @@ Attack Vectors: Incorrect handling can lead to MIME type confusion attacks.
 `,
 		option16: `
 sample:
-	Cookie: _ga_W7DW3R7679=GS1.1.1728331726.2.0.1728331726.0.0.0; _ga=GA1.1.1721535675.1728328576;
+	cookie: _ga_W7DW3R7679=GS1.1.1728331726.2.0.1728331726.0.0.0; _ga=GA1.1.1721535675.1728328576;
 
 Usage: Stores session data on the client.
 
@@ -186,7 +186,7 @@ sample:
 	
 		option20: `
 sample:
-	Date: Tue, 08 Oct 2024 02:25:23 GMT
+	date: Tue, 08 Oct 2024 02:25:23 GMT
 
 Usage: Represents the date and time at which the message was sent.
 
@@ -195,7 +195,7 @@ Attack Vectors: Generally low risk, used for caching and logging.
 `,
     option21: `
 sample: 
-	ETag: W/"fe-HXRZFMMFoUSWc30W70zcP6Qm7LE"
+	etag: W/"fe-HXRZFMMFoUSWc30W70zcP6Qm7LE"
 
 Usage: Provides a mechanism for cache validation.
 
@@ -219,7 +219,7 @@ sample:
 `,
 		option24: `
 sample:
-	If-None-Match: W/"1b1-GFzCGQqk9c7puNvL7NxWzMVJAR8"
+	if-none-match: W/"1b1-GFzCGQqk9c7puNvL7NxWzMVJAR8"
 
 Usage: Makes a request conditional based on ETag values.
 
@@ -228,7 +228,7 @@ Attack Vectors: Potentially used for cache poisoning if not handled correctly
 `,
     option25: `
 sample:
-	Origin: https://hackerone.com
+	origin: https://hackerone.com
 
 Usage: Indicates the origin of the request, used in CORS.
 
@@ -253,7 +253,7 @@ sample:
 	
 		option28: `
 sample:
-	Referer: https://hackerone.com/
+	referer: https://hackerone.com/
 
 Usage: Indicates the URL of the resource from which the request was initiated.
 
@@ -262,7 +262,7 @@ Attack Vectors: Can leak sensitive information if URLs contain sensitive data.
 `,
     option29: `
 sample:
-	Referrer-Policy: strict-origin-when-cross-origin
+	referrer-policy: strict-origin-when-cross-origin
 
 Usage: Governs what referrer information should be included with requests.
 
@@ -271,7 +271,7 @@ Attack Vectors: Incorrect settings can lead to information leakage
 `,
 		option30: `
 sample:
- Report-To: {"endpoints":[{"url":"https:\/\/csp-reporting.cloudflare.com\/cdn-cgi\/script_monitor\/report?m=
+ 	report-To: {"endpoints":[{"url":"https:\/\/csp-reporting.cloudflare.com\/cdn-cgi\/script_monitor\/report?m=
  j5xV2lxtvipZVAgy5s.xYjPLn3FMt2Bw8cJ1RDrBuPI-1728354323-1.0.1.1-gBE1R.ecQ2K3TirADxzDbsXOU9MQ9iVRaiFropuXzmqmHViFhyzCi9
  BynybHXR_311UsOR7AjtMO5e.WNEVFbgZqZHOmjLWYLmbuCf5zv.ZywNyXdSrgxvJ1hU8Ay6q6DjtvkD4uUvngwje.Omzg.vKXG_Qb7W8cgLE0hF5EO0g"}],
  "group":"cf-csp-endpoint","max_age":86400}
@@ -283,7 +283,7 @@ Attack Vectors: Misuse could lead to excessive or misdirected reporting.
 `,
 		option31: `
 sample:
-	Sec-Fetch-Dest: document
+	sec-fetch-dest: document
 
 Usage: Describes the destination of the request (e.g., document, script).
 
@@ -292,7 +292,7 @@ Attack Vectors: Provides context for request origins; low direct risk
 `,
     option32: `
 sample:
-	Sec-Fetch-Mode: navigate
+	sec-fetch-mode: navigate
 
 Usage: Describes the mode of the request (e.g., cors, no-cors).
 
@@ -301,7 +301,7 @@ Attack Vectors: Helps enforce security policies; low direct risk
 `,
 		option33: `
 sample:
-	Sec-Fetch-Site: none
+	sec-fetch-site: none
 
 Usage: Indicates the relationship between the origin of the request and the target.
 
@@ -310,7 +310,7 @@ Attack Vectors: Used in security checks; generally low risk
   `,
 		option34: `
 sample:
-	Sec-Fetch-User: ?1
+	sec-fetch-user: ?1
 
 Usage: Indicates user activation for a navigation request.
 
@@ -319,7 +319,7 @@ Attack Vectors: Provides context for user interactions; low direct risk
   `,
     option35: `
 sample:
-	Server: cloudflare
+	server: cloudflare
 
 Usage: Discloses information about the software used by the origin server.
 
@@ -328,7 +328,7 @@ Attack Vectors: Can provide attackers with information useful for targeting.
   `,
 		option36: `
 sample:
-	Set-Cookie:
+	set-cookie:
  
 Usage: Sends cookies from server to client, used for session management.
 
@@ -337,7 +337,7 @@ Attack Vectors: Vulnerable to session hijacking if not properly secured.
   `,
 		option37: `
 sample:
-	Strict-Transport-Security: max-age=63072000; includeSubDomains
+	strict-transport-security: max-age=63072000; includeSubDomains
 
 Usage: Enforces HTTPS, protecting against man-in-the-middle attacks.
 
@@ -353,7 +353,7 @@ sample:
 	
     option39: `
 sample:
-	Upgrade-Insecure-Requests: 1
+	upgrade-insecure-requests: 1
 
 Usage: Informs the server that the client prefers an encrypted and authenticated response.
 
@@ -362,7 +362,7 @@ Attack Vectors: Generally low risk, but ignored by non-compliant servers
   `,
     option40: `
 sample:
-	User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0
+	user-agent: Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/115.0
 
 Usage: Identifies the client software initiating the request.
 
@@ -371,7 +371,7 @@ Attack Vectors: Can be spoofed for bypassing client-specific restrictions
   `,
 		option41: `
 sample:
-	Vary: Accept-Encoding
+	vary: Accept-Encoding
 
 Usage: Informs caches which headers to consider when serving cached content.
 
@@ -380,7 +380,7 @@ Attack Vectors: Misuse can lead to cache poisoning.
   `,
 		option42: `
 sample:
-	X-Content-Type-Options: nosniff
+	x-content-type-options: nosniff
 
 Usage: Prevents MIME type sniffing, protecting against some XSS attacks.
 
@@ -389,7 +389,7 @@ Attack Vectors: Lack of this header can lead to content-type sniffing vulnerabil
   `,
     option43: `
 sample:
-	X-CSRF-Token:
+	x-csrf-token:
  
 Usage: Used to prevent Cross-Site Request Forgery (CSRF) attacks.
 
@@ -434,7 +434,7 @@ Attack Vectors: Improper settings can lead to unauthorized data access.
   `,
 		option48: `
 sample:
-	X-Request-ID:
+	x-request-id:
  
 Usage: Used for tracking and debugging requests.
 
