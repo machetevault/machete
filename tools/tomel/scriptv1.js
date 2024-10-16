588d5d18ff96b3dd43c8ed6e94141ab2bc4a406b
MACHETE VAULT <142649592+machetevault@users.noreply.github.com>
2024-10-15 23:08:34 -0600

    Delete tools/tomel/scriptv1.js

--- tools/tomel/scriptv1.js
+++
@@ -1,7 +0,0 @@
-  function showContent(contentId) {
-            const contents = document.querySelectorAll('.content div');
-            contents.forEach(content => {
-                content.classList.remove('active');
-            });
-            document.getElementById(contentId).classList.add('active');
-        }
