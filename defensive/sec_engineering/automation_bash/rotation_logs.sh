f17a8a17521b8011e8a1dafbcfed9edbb9d96845
MACHETE VAULT <142649592+machetevault@users.noreply.github.com>
2025-09-04 16:54:53 -0600

    Delete defensive directory

--- defensive/sec_engineering/automation_bash/rotation_logs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-datapath="/var/log/PATHNAME"
-archive="/var/log/PATHNAME/"
-
-#compress
-tar -czvf FILENAME_logs$(date +%Y-%m-%d).tar.gz $datapath
-#move archive
-mv /home/USER/*.tar.gz $archive
